922c2af83e92189c60ba0def97fd1ab7d4f37f0f
Khoa Pham <Khoa.Pham@keyin.com>
2025-08-03 19:20:08 -0400

    Initial commit

---
+++ golfclub-api/src/main/java/com/keyin/golfclub_api/GolfclubApiApplication.java
@@ -0,0 +1,13 @@
+package com.keyin.golfclub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfclubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfclubApiApplication.class, args);
+	}
+
+}
